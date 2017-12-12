9a2c94d1617e8359497fbec982b80a7f1399253e
Dominique Broeglin <dominique.broeglin@gmail.com>
2017-12-12 22:03:27 +0100

    Added a few more assertions to the tests

--- DSC_ConfigData/Datum.Tests.ps1
+++ DSC_ConfigData/Datum.Tests.ps1
@@ -11,11 +11,19 @@ Describe "Datum" {
             $datum = New-DatumStructure  -Structure $datumConfig
         }
 
-        It "Description" {
+        It "has AllNodes.SRV01" {
+            $datum.AllNodes.SRV01 | Should Not Be Null
+        }
+
+        It "has AllNodes.SRV01.Description" {
+            $datum.AllNodes.DEV.SRV01.Description | Should Be 'Dev version of SRV01'
+        }
+
+        It "Resolves Description" {
             Resolve-Datum -searchPaths $datumConfig.ResolutionPrecedence -DatumStructure $datum -PropertyPath 'Description' -Node $node | Should Be 'Dev version of SRV01'
         }
 
-        It "ExampleProperty1" {
+        It "Resolves ExampleProperty1" {
             Resolve-Datum -searchPaths $datumConfig.ResolutionPrecedence -DatumStructure $datum -PropertyPath 'ExampleProperty1' -Node $node | Should Be 'From Node'
         }
     }
